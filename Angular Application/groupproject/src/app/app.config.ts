08df53139bea805e5bea2e3b1c226696faf915fd
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-10-13 23:00:22 +0100

    Replaced project with angular version

---
+++ Angular Application/groupproject/src/app/app.config.ts
@@ -0,0 +1,30 @@
+import {ApplicationConfig, Component, inject, OnInit} from '@angular/core';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import {collection, Firestore, getDocs, getFirestore, provideFirestore} from '@angular/fire/firestore';
+
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() => initializeApp(
+      {"projectId":"year3groupproject-ee682",
+        "appId":"1:174849208599:web:758e24d65760a011b38752",
+        "databaseURL":"https://year3groupproject-ee682-default-rtdb.europe-west1.firebasedatabase.app",
+        "storageBucket":"year3groupproject-ee682.appspot.com",
+        "apiKey":"AIzaSyBuQ88U_qmLptVRm7IhpgrlPkNkla_4Li4",
+        "authDomain":"year3groupproject-ee682.firebaseapp.com",
+        "messagingSenderId":"174849208599",
+        "measurementId":"G-517YYC8LQR"})),
+    provideAuth(() => getAuth()),
+    provideAnalytics(() => getAnalytics()),
+    ScreenTrackingService,
+    UserTrackingService,
+    provideFirestore(() => getFirestore())]
+};
---
+++ Angular Application/groupproject/src/environments/environment.development.ts
@@ -0,0 +1,17 @@
+export const environment = {
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyBuQ88U_qmLptVRm7IhpgrlPkNkla_4Li4",
+    authDomain: "year3groupproject-ee682.firebaseapp.com",
+    databaseURL: "https://year3groupproject-ee682-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "year3groupproject-ee682",
+    storageBucket: "year3groupproject-ee682.appspot.com",
+    messagingSenderId: "174849208599",
+    appId: "1:174849208599:web:758e24d65760a011b38752",
+    measurementId: "G-517YYC8LQR"
+  }
+};
+
+
+//https://www.youtube.com/watch?v=3IXwN4XJywQ
