c5c8762f9992fcaec27c3704b0c2df610b39fa6b
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-10-14 10:30:55 +0100

    Basic Navbar Component

---
+++ Angular Application/groupproject/src/app/navbar/navbar.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.css'
+})
+export class NavbarComponent {
+  menuValue:boolean = false;
+  menu_icon :string ='bi bi-list';
+
+
+  openMenu(){
+    this.menuValue =! this.menuValue;
+    this.menu_icon = this.menuValue ? 'bi bi-x' : 'bi bi-list';
+  }
+
+  closeMenu(){
+    this.menuValue = false;
+    this.menu_icon = 'bi bi-list';
+  }
+
+}
