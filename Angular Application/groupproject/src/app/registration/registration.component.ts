b3f60c1fc3c10e69741821fd5a1f644c5fd6759e
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-10-14 21:27:38 +0100

    Registration done

--- Angular Application/groupproject/src/app/registration/registration.component.ts
+++ Angular Application/groupproject/src/app/registration/registration.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {RouterLink} from "@angular/router";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+
 
 @Component({
   selector: 'app-registration',
@@ -15,9 +17,23 @@ import {ReactiveFormsModule} from "@angular/forms";
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
-  apiForm: any;
+  registerForm: FormGroup;
 
-  onSubmit() {
-    window.location.replace('dashboard')
+  constructor(private builder: FormBuilder) {
+    this.registerForm = this.builder.group({
+      name: [''],
+      surname: [''],
+      email: [''],
+      password: ['']
+    })
+  }
+
+  registerUser() {
+    const {email, password} = this.registerForm.value;
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, email, password).then((result) => {
+      const user = result.user;
+      window.location.replace("/dashboard")
+    }).catch(console.log)
   }
 }
