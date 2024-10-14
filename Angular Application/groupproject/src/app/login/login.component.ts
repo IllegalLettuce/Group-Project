d1fb49cd19e4a827ae14a024453647b9ad120ff7
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-10-14 21:17:58 +0100

    Email login works

--- Angular Application/groupproject/src/app/login/login.component.ts
+++ Angular Application/groupproject/src/app/login/login.component.ts
@@ -1,24 +1,48 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
-import {getAuth, GoogleAuthProvider, signInWithPopup} from "firebase/auth";
+import {getAuth, GoogleAuthProvider, signInWithPopup, signInWithEmailAndPassword} from "firebase/auth";
+import {app} from "../../../server";
+import {FormBuilder, FormGroup, ReactiveFormsModule} from "@angular/forms";
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-    imports: [
-        RouterLink
-    ],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginForm: FormGroup;
+
+  constructor(private builder: FormBuilder) {
+    this.loginForm = this.builder.group({
+      email: [''],
+      password: ['']
+    })
+  }
+
+
+
   provider = new GoogleAuthProvider();
   googleLogin(){
     const auth = getAuth();
     signInWithPopup(auth, this.provider).then((result) =>{
       const user = result.user;
       window.location.replace('/dashboard')
-    })
+    }).catch(console.log)
   }
 
+  emailLogin() {
+      const {email, password} = this.loginForm.value;
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, email, password).then((result) => {
+        const user = result.user;
+        window.location.replace("/dashboard")
+      }).catch(console.log)
+    }
 }
--- Angular Application/groupproject/src/app/registration/registration.component.ts
+++ Angular Application/groupproject/src/app/registration/registration.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {RouterLink} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-registration',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterLink,
+    ReactiveFormsModule
   ],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.css'
 })
 export class RegistrationComponent {
+  apiForm: any;
 
+  onSubmit() {
+    window.location.replace('dashboard')
+  }
 }
