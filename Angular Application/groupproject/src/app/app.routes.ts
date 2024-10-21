e364b62cd6956fe5298d87b5e4e57cfd5ea140ca
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-10-21 22:31:03 +0100

    Help form, connection to firestorm

--- Angular Application/groupproject/src/app/app.routes.ts
+++ Angular Application/groupproject/src/app/app.routes.ts
@@ -1,15 +1,16 @@
-import {RouterModule, Routes} from '@angular/router';
+import {Routes} from '@angular/router';
 import {RegistrationComponent} from "./registration/registration.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginComponent} from "./login/login.component";
+import {HelpsupportComponent} from "./helpsupport/helpsupport.component";
 
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'registration', component: RegistrationComponent},
   {path: 'dashboard', component: DashboardComponent},
-  {path: 'helpsupport', component: DashboardComponent},
-  {path: '**', redirectTo: '/'}
+  {path: 'helpsupport', component: HelpsupportComponent},
+  {path: 'registration', component: RegistrationComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 
--- Angular Application/groupproject/src/app/helpsupport/helpsupport.component.ts
+++ Angular Application/groupproject/src/app/helpsupport/helpsupport.component.ts
@@ -1,15 +1,44 @@
+// https://medium.com/@0ka/angular-firebase-create-and-read-e1fa37494f30
 import { Component } from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {RouterOutlet} from "@angular/router";
+import {FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {NgIf} from "@angular/common";
+import {addDoc, collection, Firestore} from "@angular/fire/firestore";
 
 @Component({
   selector: 'app-helpsupport',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    RouterOutlet,
+    FormsModule,
+    NgIf,
+    ReactiveFormsModule
   ],
   templateUrl: './helpsupport.component.html',
   styleUrl: './helpsupport.component.css'
 })
 export class HelpsupportComponent {
+  helpForm: FormGroup;
 
+  constructor(private builder: FormBuilder, public firestore: Firestore) {
+    this.helpForm = this.builder.group({
+      title: ['', Validators.required],
+      message: ['', Validators.required]
+    });
+  }
+
+  async createHelpMsg(title: string, message: string) {
+    const docRef = await addDoc(collection(this.firestore, 'help_message'), {
+      title: title,
+      message: message
+    });
+  }
+
+  submitHelp() {
+    const {title, message} = this.helpForm.value;
+    this.createHelpMsg(title, message)
+      .then(r => window.location.replace('dashboard'));
+  }
 }
