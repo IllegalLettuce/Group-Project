adb3508cd0d2bdd447499aa2444e05064a505c28
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-11-11 12:05:40 +0000

    Successful API call to LLM implementation

--- Angular Application/groupproject/src/app/dashboard/dashboard.component.ts
+++ Angular Application/groupproject/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,52 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+
+  httpClient = inject(HttpClient);
+  public data: Array<any> = [];
+
+
+  ngOnInit(){
+
+  }
+
+  getStockNameFromUser(name: String){
+    console.log(name)
+    this.sendStockNameToLLM(name)
+  }
+
+
+  async sendStockNameToLLM(name:String){
+    let message:JSON = <JSON><unknown>{
+      "company": name
+    }
+    const myJSON = JSON.stringify(message);
+
+    this.httpClient.post('https://quiet-yak-presently.ngrok-free.app', name)
+      .subscribe({
+        next: (data: any) => {
+          console.log(data.blog);
+          console.log(data.recommendation);
+          this.data = data;
+        }, error: (err) => console.log(err)
+      });
+  }
+
+
+
+
+
+
 
 }
