f4305c5e500e9173d3fcf1f4fcf340e33970d38c
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-11-18 12:07:28 +0000

    Updated Model Dialog

--- Angular Application/groupproject/src/app/dashboard/dashboard.component.ts
+++ Angular Application/groupproject/src/app/dashboard/dashboard.component.ts
@@ -1,12 +1,11 @@
-import {Component, inject} from '@angular/core';
+import {Component} from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
-import {HttpClient} from "@angular/common/http";
 import {FormsModule} from "@angular/forms";
-import {environment} from "../../environments/environment.development";
 import {NgForOf} from "@angular/common";
 import {MatDialog, MatDialogModule} from '@angular/material/dialog';
 import {ReportmodalComponent} from "./modals/reportmodal/reportmodal.component";
 import {CommonModule} from "@angular/common";
+import {ManagemodalComponent} from "./modals/managemodal/managemodal.component";
 
 @Component({
   selector: 'app-dashboard',
@@ -23,57 +22,35 @@ import {CommonModule} from "@angular/common";
 })
 export class DashboardComponent {
 
-  httpClient = inject(HttpClient);
-  public data: Array<any> = [];
-  buy: any;
-  sell: any ;
-
-  constructor(private dialog: MatDialog) {}
-
+  public data:any;
   stocks = [
     { name: 'Lockheed Martin', buy: 0, sell: 0 },
     { name: 'Tesla', buy: 0, sell: 0 },
     { name: 'Apple', buy: 0, sell: 0 },
     // Add more companies as needed
   ];
+  constructor(private dialog: MatDialog) {}
 
+  /**
+   * Controls the report dialog from the frontend
+   * @param name
+   */
   openReportDialog(name: string){
     this.dialog.open(ReportmodalComponent, {
-      width: '250px',
+      width: '24em',
+      data: { name: name }
+    })
+  };
+
+  /**
+   * Controls the manage stocks dialog from the frontend
+   * @param name
+   */
+  openManageDialog(name: string){
+    this.dialog.open(ManagemodalComponent, {
+      width: '24em',
       data: { name: name }
     })
   }
-
-
-    generateReport(name: String){
-    console.log("Generating report for", name);
-  }
-
-  //takes stock data from the form
-  submitStockData(stockName: String, buy: any, sell: any){
-    this.sendToLLM(stockName, buy, sell, 1000)
-  }
-
-  //sends stock data to API as JSON
-  sendToLLM(name:String, buy: any, sell: any, funds_dollar: any ){
-
-    let message:JSON = <JSON><unknown>{
-      "company": name,
-      "buy_percent": buy,
-      "sell_percent": sell,
-      "funds_dollar": funds_dollar,
-    };
-
-    const uri = environment.API_BASE_URL;
-    this.httpClient.post(uri, message)
-      .subscribe({
-        next: (data: any) => {
-          this.data = data;
-          console.log(data)
-        }, error: (err) => console.log(err)
-      });
-  }
-
 /////////////////////////end of file
-
 }
