fbb3668c5c67ce014ac6a48a213abc41ae928557
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-11-27 17:17:06 +0000

    Route authentication

--- Angular Application/groupproject/src/app/app.routes.ts
+++ Angular Application/groupproject/src/app/app.routes.ts
@@ -3,13 +3,16 @@ import {RegistrationComponent} from "./registration/registration.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
 import {LoginComponent} from "./login/login.component";
 import {HelpsupportComponent} from "./helpsupport/helpsupport.component";
+import {DashboardmanagerComponent} from "./dashboardmanager/dashboardmanager.component";
+import {authGuard} from "./auth.guard";
 
 
 export const routes: Routes = [
   {path: '', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: 'helpsupport', component: HelpsupportComponent},
   {path: 'registration', component: RegistrationComponent},
+  {path: 'dashboard', component: DashboardComponent, canActivate:[authGuard]},
+  {path: 'dasboardmanager', component: DashboardmanagerComponent, canActivate:[authGuard]},
+  {path: 'helpsupport', component: HelpsupportComponent, canActivate:[authGuard]},
   {path: '**', redirectTo: ''}
 ];
 
