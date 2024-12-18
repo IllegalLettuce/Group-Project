0142837737dfb4889e71def2bff4bf9253106344
ferenc <161438972+wazkovics@users.noreply.github.com>
2024-12-18 18:42:31 +0000

    Added autopurchase code
    Auto purchase now works

--- Backend LLM/LLM/flaskapptest.py
+++ Backend LLM/LLM/flaskapptest.py
@@ -2,6 +2,7 @@ import os
 
 import json
 import time
+from threading import Thread
 
 from google.cloud.firestore_v1 import FieldFilter
 from sympy.testing.runtests import method
@@ -135,7 +136,7 @@ blogger_agent = Agent(
 
 autopurchase_agent = Agent(
     role="Stock analysis and future predictor",
-    goal="Give the predicted buy, sell and hold percentages ",
+    goal="Give the predicted buy and sell ",
     backstory="Enjoys looking at stock analysis from people and coming up with their own prediction ",
     llm=ollama3
 )
@@ -478,14 +479,15 @@ def autopurchaseregister():
         datainput = json.loads(request.data.decode('utf-8'))
         stockName = datainput.get('name')
         ticker = datainput.get('Ticker')
-        funds = datainput.get('funds')
+        funds = int(datainput.get('funds'))
         userid = datainput.get('userId')
-        buy = datainput.get('buy')
-        sell = datainput.get('sell')
+        buy = int(datainput.get('buy'))
+        sell = int(datainput.get('sell'))
         shares_owned = 0
 
         try:
-            db.collection("autopurchase").document().create({"user_id":userid , "name":stockName,"ticker":ticker, "shares_owned": shares_owned,"funds":funds,"buy":buy,"sell":sell})
+            db.collection("autopurchase").document().create({"userid":userid , "company":stockName,"ticker":ticker, "shares_owned": shares_owned,"buy_percent":buy,"sell_percent":sell})
+            db.collection("funds").document().create({"userid":userid, "funds":funds})
             return jsonify({
                 "status": "Success",
                 "message": "Autopurchase set up"
@@ -774,7 +776,97 @@ def getstocks():
         return jsonify(stocks), 200
 
 
+
+def autopurchase():
+
+    while(True):
+
+        # Load the stock data from the JSON file
+        # main_stock_data()
+
+        with open('stock_data.json', 'r') as json_file:
+            stock_data = json.load(json_file)
+
+
+        # time.sleep((60*60*2)) for a constant loop put this around the code below
+        try:
+            docs = (
+                db.collection("autopurchase").stream()
+            )
+
+            docs = list(docs)
+
+            if docs:
+                for doc in docs:
+                    shares_owned = doc.get('shares_owned')
+                    document_id = doc.id
+                    company = doc.get('name')
+                    buy = doc.get('buy_percent')
+                    sell = doc.get('sell_percent')
+                    funds = 0
+                    user_id = doc.get('userid')
+                    fundsid = 0
+
+                    funddocs = (
+                        db.collection("funds").stream()
+                    )
+
+                    funddocs = list(funddocs)
+                    for funds in funddocs:
+                        if funds.get('userid') == user_id:
+                            funds.get('funds')
+                            fundsid = funds.id
+
+
+                    company_stock = stock_data[company]
+                    lastitemdate = list(company_stock)[-1]
+                    closingorice = company_stock[lastitemdate]['Close']
+                    autopurchase_task = Task(
+                        description="From the data given here make a prediction as to whether it is a good idea to buy or sell the stock of this given company "+ str(company_stock),
+                        agent=autopurchase_agent,
+                        expected_output="I expect the output to be given like this as whole numbers, do not put string, just numbers even if its 0 and nothing more."
+                                        "{"
+                                        '"Buy"": "x%",'
+                                        '"Sell": "z%"'
+                                        "}"
+                    )
+                    crew = Crew(agents=[autopurchase_agent],
+                                tasks=[autopurchase_task], verbose=True)
+                    result = crew.kickoff()
+                    result = str(result)
+                    result = result.replace("%", "")
+                    result = result.replace("-", "")
+                    result = result.replace("+", "")
+                    print(result)
+                    jsonobject = json.loads(result)
+                    agent_buy = int(jsonobject.get('Buy'))
+                    agent_sell = int(jsonobject.get('Sell'))
+                    #if the buy from agent is greater than the given from user, then go ahead and buy as many stocks as possible until you can buy anymore
+                    #then save that into the firebase database
+
+                    if int(agent_buy)>=buy and funds > closingorice:
+                        shares_owned  += funds / closingorice
+                        funds -= shares_owned*closingorice
+                        db.collection("autopurchase").document(document_id).set({ "shares_owned": shares_owned}, merge=True)
+                        db.collection("funds").document(fundsid).set({"funds":funds}, merge=True)
+
+                        #save share bought , ticker, user id into database so it can be retrieved for future use
+                    else:
+                        if agent_sell >= sell and shares_owned > 0:
+                            #get the share from database and sell them all
+                            newfunds = closingorice * shares_owned
+                            shares_owned = 0
+                            db.collection("autopurchase").document(document_id).set({ "shares_owned": shares_owned}, merge=True)
+                            db.collection("funds").document(fundsid).set({"funds":newfunds}, merge=True)
+        except Exception as e:
+            logging.error(f"Error during task execution: {e}")
+        time.sleep(1000)
+
+
 if __name__ == '__main__':
     # no reason to run it always, we'll make it a app route call but we might not even use it
-
-    app.run(debug=True)
+    # main_stock_data()
+    thread = Thread(target = autopurchase,args = {})
+    thread.start()
+    thread.join()
+    # app.run(debug=True)
