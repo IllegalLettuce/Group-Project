9de108f753575372dedb33d889d7da9ceb4701bd
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-12-20 18:22:59 +0000

    Navbar has admins page now

--- Angular Application/groupproject/src/app/navbar/navbar.component.ts
+++ Angular Application/groupproject/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {RouterLink} from "@angular/router";
 import { getAuth, signOut } from "firebase/auth";
+import {UserCheckService} from "../services/user-check.service";
+import {NgIf} from "@angular/common";
 
 
 
@@ -8,7 +10,8 @@ import { getAuth, signOut } from "firebase/auth";
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    NgIf
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
@@ -17,10 +20,26 @@ export class NavbarComponent implements OnInit{
   auth = getAuth();
   user = this.auth.currentUser;
   username = this.user?.displayName;
+  isManager = false;
+  isAdmin = false;
+
+
+  constructor(
+    private userCheck: UserCheckService,
+  ) {}
 
   ngOnInit(){
     if (!this.username){
       this.username = "Userpage";
+    }
+    this.getUserType()
+  }
+
+  async getUserType(){
+    const userID = this.user?.uid;
+    if (userID != null) {
+      this.isAdmin = await this.userCheck.isUserAnAdmin(userID);
+      this.isManager = await this.userCheck.isUserAnManager(userID);
     }
   }
 
