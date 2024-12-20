781c133e278ace1399ad0632f0d0149ba35d2ad7
Anthony Sabbe <100137579+IllegalLettuce@users.noreply.github.com>
2024-12-20 11:56:39 +0000

    Modified to user usercheck service, now shows message for admin and manager

--- Angular Application/groupproject/src/app/userpage/userpage.component.ts
+++ Angular Application/groupproject/src/app/userpage/userpage.component.ts
@@ -2,32 +2,47 @@ import {Component, OnInit} from '@angular/core';
 import {NavbarComponent} from "../navbar/navbar.component";
 import {getAuth} from "firebase/auth";
 import {doc, Firestore, getDoc, getFirestore} from "@angular/fire/firestore";
+import {UserCheckService} from "../services/user-check.service";
+import {Auth} from "@angular/fire/auth";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-userpage',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    NgIf
   ],
   templateUrl: './userpage.component.html',
   styleUrl: './userpage.component.css'
 })
 export class UserpageComponent implements OnInit {
+  isAdmin: boolean  | undefined;
+  isManager: boolean | undefined;
 
-  constructor(private firestore: Firestore) {}
+  constructor(
+    private firestore: Firestore,
+    private userCheck: UserCheckService) {}
 
-  ngOnInit(): void {
-    this.fetchUserData();
-  }
-
-  async fetchUserData() {
+  async ngOnInit() {
+    this.isAdmin  = false;
+    this.isManager = false;
     const auth = getAuth();
     const user = auth.currentUser;
+    const userID = user?.uid;
+    if (userID != null) {
+      this.isAdmin = await this.userCheck.isUserAnAdmin(userID);
+      this.isManager = await this.userCheck.isUserAnManager(userID);
+      console.log("Admin: ", this.isAdmin);
+      console.log("Manager: ", this.isManager);
+    }
+    this.fetchUserData(auth, user);
+  }
 
+  async fetchUserData(auth: Auth, user: any) {
     if (user) {
       const userRef = doc(this.firestore, "users", user.uid);
       const userDoc = await getDoc(userRef);
-
       if (userDoc.exists()) {
         const userData = userDoc.data();
         console.log("User Role:", userData['userType']);
